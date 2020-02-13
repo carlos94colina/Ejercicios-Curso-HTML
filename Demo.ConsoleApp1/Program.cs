5b71293720a6942d27b610d32fd7adafb2a2b30c
Borja Cabeza <borjacabezarozas@gmail.com>
2020-02-13 16:23:54 +0100

    Commit - Demos CSharp 2

--- Demo.ConsoleApp1/Program.cs
+++ Demo.ConsoleApp1/Program.cs
@@ -13,9 +13,63 @@ namespace Demo.ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Alumno[] alumnos = new Alumno[5];
+            Alumno alumnoDemo = new Alumno()
+            {
+                nombre = "Borja",
+                Apellido1 = "Cabeza",
+                Edad = 19
+            };
 
+            float[] notas = new float[] { 5.98F, 6.74F, 10F, 5.09F, 8.99F, 7.0F };
+
+            Console.WriteLine("Mayor de Edad: " + alumnoDemo.MayorDeEdad(21));
+            Console.WriteLine("Media: " + alumnoDemo.CalcularMedia(notas));
+
+            Console.ReadKey();
+
+            ////////////////////////////////////////////////////////////////
+
+            Alumno[] alumnos = new Alumno[5];
             alumnos[2] = new Alumno() { nombre = "Ana María", Apellido1 = "Sanz" };
+
+            var alumnos2 = new Alumno[] {
+                new Alumno() { nombre = "Ana María", Apellido1 = "Sanz" },
+                new Alumno() { nombre = "María", Apellido1 = "Ruiz" },
+                new Alumno() { nombre = "Carlos", Apellido1 = "Sánchez" },
+                new Alumno() { nombre = "Julian", Apellido1 = "García" }
+            };
+
+            for (var c = 0; c < alumnos2.Length; c++)
+            {
+                Console.WriteLine("Posición: {0} - Valor: {1}", 
+                    
+                    
+                    c, alumnos2[c].nombre + " " + alumnos2[c].Apellido1);
+            }
+
+            foreach (var item in alumnos2)
+            {
+                Console.WriteLine("Valor: {0}", item.nombre + " " + item.Apellido1);
+            }
+
+
+
+
+            var dias = new string[] {
+                "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado", "Domingo"
+            };
+
+            for (var c = 0; c < dias.Length; c++)
+            {
+                Console.WriteLine("Posición: {0} - Valor: {1}", c, dias[c]);
+            }
+
+            foreach(var dia in dias)
+            {
+                Console.WriteLine("Valor: {0}", dia);
+            }
+
+
 
 
             Console.ReadKey();
@@ -83,10 +137,20 @@ namespace Universidad
         public string Apellido2 { get; set; }
         public byte Edad { get; set; }
         public void Limpiar()
-        { }
-        public bool MayorDeEdad()
+        { 
+        
+        }
+        public bool MayorDeEdad(int limite)
         {
-            return true;
+            if (Edad >= limite) return true;
+            else return false;
+        }
+
+        public float CalcularMedia(float[] notas)
+        { 
+            float media = 0;
+
+            return media;
         }
 
         public Alumno()
