602c88e175bac2e5ddfd737e05157aa685530596
Borja Cabeza <borjacabezarozas@gmail.com>
2020-02-18 15:41:09 +0100

    Commit - Ejercicio de Vocales Finalizado

--- Lab.Northwind.WebApplication1/api/comandos.ashx.cs
+++ Lab.Northwind.WebApplication1/api/comandos.ashx.cs
@@ -23,7 +23,34 @@ namespace Lab.Northwind.WebApplication1.api
                     resultado = DateTime.Now.ToString();
                     break;
                 case "vocales":
-                    var array = texto.ToArray<char>();
+                    var array = texto.ToLower().ToArray<char>();
+
+                    int contador = 0;
+
+                    foreach (var letra in array)
+                    {
+                        if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') contador++;
+                    }
+
+                    resultado = contador.ToString();
+
+                    //////////////////////////////////////////////////////////////////////////////
+
+                    char[] vocales = new char[] { 'a', 'e', 'i', 'o', 'u' };
+
+                    int contador2 = 0;
+
+                    foreach (var letra in array)
+                    {
+                        if (vocales.Contains(letra)) contador2++;
+                    }
+
+                    resultado = contador.ToString();
+
+                    //////////////////////////////////////////////////////////////////////////////
+
+                    char[] vocales2 = new char[] { 'a', 'e', 'i', 'o', 'u' };
+                    resultado = array.Count(r => vocales2.Contains(r)).ToString();
 
                     break;
                 default:
