e75ab7e6bbe0f340ad23f820a09b4daec04633d5
Borja Cabeza <borjacabezarozas@gmail.com>
2020-02-18 15:18:40 +0100

    Commit - Ejercicio Vocales

--- Lab.Northwind.WebApplication1/api/comandos.ashx.cs
+++ Lab.Northwind.WebApplication1/api/comandos.ashx.cs
@@ -10,10 +10,29 @@ namespace Lab.Northwind.WebApplication1.api
 
         public void ProcessRequest(HttpContext context)
         {
+            string texto = context.Request.Params["texto"];
+            string comando = context.Request.Params["comando"];
+            string resultado = "";
 
+            switch (comando)
+            {
+                case "cuenta":
+                    resultado = texto.Length.ToString();
+                    break;
+                case "fecha":
+                    resultado = DateTime.Now.ToString();
+                    break;
+                case "vocales":
+                    var array = texto.ToArray<char>();
+
+                    break;
+                default:
+                    resultado = "Comando no valido";
+                    break;
+            }
 
             context.Response.ContentType = "text/plain";
-            context.Response.Write("Hola a todos");
+            context.Response.Write(resultado);
         }
 
         public bool IsReusable
